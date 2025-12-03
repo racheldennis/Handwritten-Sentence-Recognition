e09480d12896ae3ff9b2de03e22da94992fe824f
John Ibrahim <john.ibrahim119@gmail.com>
2025-12-03 17:56:18 -0500

    feat: added data split and dataloaders

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
+from sklearn.model_selection import train_test_split
+
 from dataset import HandwrittenSentenceDataset
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device: {device}")
+
+## Load data
 
 LABEL_PATH = os.path.join("data", "ascii", "sentences.txt")
 IMAGE_FOLDER_PATH = os.path.join("data", "sentences")
@@ -37,5 +41,39 @@ with open(LABEL_PATH, "r") as file:
         images.append(image_path)
         labels.append(label)
 
-print(len(images), "images found.")
-print(len(labels), "labels found.")
+print("|", len(images), "images found.")
+print("|", len(labels), "labels found.")
+
+## Split the data into training, validation, and test sets
+
+print("\nSplitting data:")
+
+images_train, images_temp, labels_train, labels_temp = train_test_split(images, labels, test_size=0.2)
+images_val, images_test, labels_val, labels_test = train_test_split(images_temp, labels_temp, test_size=0.5)
+
+print("| Data split.")
+
+## Create datasets
+
+print("\nCreating datasets:")
+
+train_dataset = HandwrittenSentenceDataset(images_train, labels_train)
+val_dataset = HandwrittenSentenceDataset(images_val, labels_val)
+test_dataset = HandwrittenSentenceDataset(images_test, labels_test)
+
+print("| Datasets ceated.")
+
+## Create dataloaders
+
+BATCH_SIZE = 32
+
+print("\nCreating dataloaders:")
+
+train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+print(f"| Training dataloader: {len(train_loader.dataset)} samples in {len(train_loader)} batches of size {BATCH_SIZE}")
+print(f"| Validation dataloader: {len(val_loader.dataset)} samples in {len(val_loader)} batches of size {BATCH_SIZE}")
+print(f"| Test dataloader: {len(test_loader.dataset)} samples in {len(test_loader)} batches of size {BATCH_SIZE}")
+print("Dataloaders created.")
